1582410d837e4a4ca9e1e1dd2aa5552897331733
carpenox <68877971+carpenox@users.noreply.github.com>
2025-05-05 10:50:34 -0400

    Update certbot.sh

--- certbot.sh
+++ certbot.sh
@@ -1,23 +1,11 @@
-
-#!/bin/bash
-
-# DNS name to resolve
-DNS_NAME="acme-v02.api.letsencrypt.org"
-
-# Get the current IPs
-CURRENT_IPS=$(dig +short $DNS_NAME)
-
-# Remove old rules for this DNS name
-#firewall-cmd --zone=public --remove-rich-rule='rule family="ipv4" source address="ALL_OLD_IP_ADDRESSES" accept' --permane>
-
-# Add new rules for the current IPs
-for IP in $CURRENT_IPS; do
-  firewall-cmd --zone=trusted --add-rich-rule="rule family='ipv4' source address='$IP' accept" --permanent
-done
-
-# Reload firewalld to apply the changes
-firewall-cmd --reload
-
+#rm -rf /etc/letsencrypt/live/*.*
+#rm -rf /etc/letsencrypt/archive/*.*
+rm -rf /etc/httpd/conf.d/*-le-ssl.conf
+mv /etc/httpd/conf.d/viciportal-ssl.conf /etc/httpd/conf.d/viciportal-ssl.conf.
 service firewalld stop
 certbot renew
-service firewalld start
+service firewalld restart
+mv /etc/httpd/conf.d/viciportal-ssl.conf. /etc/httpd/conf.d/viciportal-ssl.conf
+domain=$(basename /etc/letsencrypt/renewal/*.conf | sed 's/\.conf$//')
+cp "/etc/letsencrypt/live/${domain}-*" "/etc/letsencrypt/live/${domain}" -r
+systemctl reload httpd
