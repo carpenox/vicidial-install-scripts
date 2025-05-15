e3d5623b1489b466cfc0a3ba5b0b9c67d4781662
carpenox <68877971+carpenox@users.noreply.github.com>
2025-05-15 11:40:45 -0400

    Update alma-rocky-centos-9-ast18-contabo.sh

--- alma-rocky-centos-9-ast18-contabo.sh
+++ alma-rocky-centos-9-ast18-contabo.sh
@@ -771,7 +771,7 @@ Thank you for choosing CyburDial and carpenox's auto installer!
 Visit our Knowledge Base at https://www.dialer.one
 
 Support: info@dialer.one
-Skype Live Chat Support: https://join.skype.com/ujkQ7i5lV78O
+Discord Live Chat Support: https://discord.gg/ymGZJvF6hK
 EOF
 
 #add rc-local as a service - thx to ras
